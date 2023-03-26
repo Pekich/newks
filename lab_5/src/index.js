b370894de0180a406adf790e1ef7a984a40d6b6d
Pekich <93043835+Pekich@users.noreply.github.com>
2023-03-26 23:48:48 +0300

    Add files via upload

---
+++ lab_5/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    }
+  }
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
---
+++ lab_5/src/shared/constants.js
@@ -0,0 +1,25 @@
+export const POSTS_URL = 'https://5fb3db44b6601200168f7fba.mockapi.io/api/posts/';
+
+export const POSTS = [
+  {
+    id: 1,
+    title: "Post 1",
+    description:
+      "Мехмат семья",
+    liked: false,
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    description:
+      "Компьютерные сети",
+    liked: false,
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    description:
+      "Это 5ая лаба",
+    liked: false,
+  },
+];
